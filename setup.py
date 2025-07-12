b6979f9d62cc6b70be476547f428a3160363b7f1
Jesse Dingley <jessedingley@Jesses-MacBook-Pro.local>
2025-07-12 18:01:26 +0700

    ent pt cmd

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 # setup.py
 from setuptools import setup
-from setuptools.command.install import install
-import os
-
-class PostInstallCommand(install):
-    """Post-installation to patch streamlit."""
-    def run(self):
-        import patch  # this must import and run the patch function
-        print("hello")
-        super().run()
 
 setup(
     name="streamlit_id_token_patch",
     version="0.1",
+    py_modules=["patch"],
     data_files=[("site-packages", ["patch.pth"])],
-    py_modules=["patch"],  # assuming patch.py is in the root
-    cmdclass={
-        'install': PostInstallCommand,
-    },
+    description="Monkey patch for Streamlit to expose ID token",
 )
